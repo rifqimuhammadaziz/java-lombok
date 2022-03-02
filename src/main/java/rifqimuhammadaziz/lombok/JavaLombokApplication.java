f8b1a344ec9ddc7c6cec7e425bdc6554528dfe8d
Rifqi Muhammad Aziz <rifqimuhammadaziz@gmail.com>
2022-03-02 14:23:00 +0700

    create new project & add dependencies

---
+++ src/main/java/rifqimuhammadaziz/lombok/JavaLombokApplication.java
@@ -0,0 +1,13 @@
+package rifqimuhammadaziz.lombok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaLombokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaLombokApplication.class, args);
+	}
+
+}
