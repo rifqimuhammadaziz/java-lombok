4fe83676c6693b24e82b45d63b389073523e700e
Rifqi Muhammad Aziz <rifqimuhammadaziz@gmail.com>
2022-03-03 14:34:07 +0700

    add Slf4j annotation in class to auto generate log

--- src/main/java/rifqimuhammadaziz/lombok/FileHelper.java
+++ src/main/java/rifqimuhammadaziz/lombok/FileHelper.java
@@ -2,15 +2,18 @@ package rifqimuhammadaziz.lombok;
 
 import lombok.Cleanup;
 import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 
 import java.io.FileReader;
 import java.util.Scanner;
 
+@Slf4j // generate log field (static)
 public class FileHelper {
 
     // auto generate exception (convert throwable to RuntimeException, but not changed initial exception), no need to try catch
     @SneakyThrows
     public static String loadFile(String file) {
+        log.info("Load file {}", file);
         @Cleanup FileReader fileReader = new FileReader(file); // auto close resource
         @Cleanup Scanner scanner = new Scanner(fileReader); // auto close resource
 
