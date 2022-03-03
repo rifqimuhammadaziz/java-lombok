30e61ce0bfddd67ded06bd40380b65163edeb7ee
Rifqi Muhammad Aziz <rifqimuhammadaziz@gmail.com>
2022-03-03 11:45:29 +0700

    add SneakyThrows annotation to generate expception

--- src/main/java/rifqimuhammadaziz/lombok/FileHelper.java
+++ src/main/java/rifqimuhammadaziz/lombok/FileHelper.java
@@ -1,13 +1,16 @@
 package rifqimuhammadaziz.lombok;
 
 import lombok.Cleanup;
+import lombok.SneakyThrows;
 
 import java.io.FileReader;
 import java.util.Scanner;
 
 public class FileHelper {
 
-    public static String loadFile(String file) throws Exception{
+    // auto generate exception (convert throwable to RuntimeException, but not changed initial exception), no need to try catch
+    @SneakyThrows
+    public static String loadFile(String file) {
         @Cleanup FileReader fileReader = new FileReader(file); // auto close resource
         @Cleanup Scanner scanner = new Scanner(fileReader); // auto close resource
 
